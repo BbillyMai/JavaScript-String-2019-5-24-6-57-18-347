33bf2ecb5b87f68e925c6cb261867d7c78dfea1b
BbillyMai <1363543881@qq.com>
2019-06-15 12:42:04 +0800

    javascript assignment

--- index.js
+++ index.js
@@ -4,13 +4,17 @@
 // 完成下面程序，将`name`变量中的字母全部转为为大写，输出：`'HELLO'`。
 var name = 'hello';
 // TODO write your code here...
-
+console.log(name.toUpperCase());
 
 // 完成下面程序，将sentence变量中的所有单词首字母变为大写，输出：'Good Afternoon, Mr Mike.'。
 var sentence = 'good afternoon, mr mike.';
 //TODO write your code here...
-
+var newSentence=sentence.replace(/\b\w+\b/g, function(word){
+    return word.substring(0,1).toUpperCase()+word.substring(1);}
+    );
+console.log(newSentence);
 
 // 完成下面程序，将money变量中的金额提取出来，输出：20。
 var money = '￥20';
 //TODO write your code here...
+console.log(money.substr(1));
